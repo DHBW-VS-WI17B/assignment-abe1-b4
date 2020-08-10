60c80c816561af47bd6a9d32fbe39eaa0ea75523
Simon Eßlinger <github@esslinger.dev>
2020-08-10 21:10:57 +0200

    feat(server): add entities (#8)
    * feat(server): add entities
    
    * chore(server): add missing comment

---
+++ src/server/TicketStore.Server.Entities/Event.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TicketStore.Server.Entities
+{
+    /// <summary>
+    /// Represents an event.
+    /// </summary>
+    public class Event
+    {
+        /// <summary>
+        /// Unique id.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Name of the event.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// When the event takes place.
+        /// </summary>
+        public DateTimeOffset Date { get; set; }
+
+        /// <summary>
+        /// Where the event takes place.
+        /// </summary>
+        public string Location { get; set; }
+
+        /// <summary>
+        /// Price of one ticket for the event.
+        /// </summary>
+        public float PricePerTicket { get; set; }
+
+        /// <summary>
+        /// Maximum count of tickets available.
+        /// </summary>
+        public int MaxTicketCount { get; set; }
+
+        /// <summary>
+        /// Maximum count of tickets a customer can buy.
+        /// </summary>
+        public int MaxTicketsPerCustomer { get; set; }
+
+        /// <summary>
+        /// Sales start date.
+        /// </summary>
+        public DateTimeOffset SalesStartDate { get; set; }
+
+        /// <summary>
+        /// Duration of a sale.
+        /// </summary>
+        public TimeSpan SaleDuration { get; set; }
+
+        /// <summary>
+        /// List of tickets sold to customers.
+        /// </summary>
+        public IEnumerable<Dictionary<Customer, Ticket>> SoldTickets { get; set; }
+    }
+}
+    
---
+++ src/server/TicketStore.Server.Entities/Ticket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketStore.Server.Entities
+{
+    /// <summary>
+    /// Ticket for an <see cref="TicketStore.Server.Entities.Event"/>.
+    /// </summary>
+    public class Ticket
+    {
+        /// <summary>
+        /// Unique id.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// When the ticket was purchased
+        /// </summary>
+        public DateTimeOffset PurchaseDate { get; set; }
+    }
+}
