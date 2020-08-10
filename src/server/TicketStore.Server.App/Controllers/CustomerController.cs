6933d64b69c26e264c87d5ecaec696076c5898c7
skyfrk <github@esslinger.dev>
2020-08-10 22:30:12 +0200

    feat(server): add asp.net server

---
+++ src/server/TicketStore.Server.App/Controllers/CustomerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TicketStore.Server.App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        /// <summary>
+        /// Gets a list of purchased tickets.
+        /// </summary>
+        /// <param name="customerId">Customer id.</param>
+        /// <param name="sort">Sort by either "orderDate" or "eventDate".</param>
+        /// <param name="order">Order ascending ("asc") oder descending ("desc").</param>
+        /// <returns>Returns a list of tickets.</returns>
+        [HttpGet]
+        [Route("/{customerId}/tickets")]
+        public async Task<IActionResult> GetTickets(Guid customerId, [FromQuery] string sort, [FromQuery] string order)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary>
+        /// Gets the budget of a customer.
+        /// Only available for customers.
+        /// </summary>
+        /// <param name="customerId">Customer id.</param>
+        /// <returns>Budget of a customer</returns>
+        [HttpGet]
+        [Route("/{customerId}/budget")]
+        public async Task<IActionResult> GetBudget(Guid customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ src/server/TicketStore.Server.App/Controllers/EventsController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TicketStore.Server.Entities;
+
+namespace TicketStore.Server.App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        /// <summary>
+        /// Get events.
+        /// </summary>
+        /// <returns>List of available events as eventId and eventName.</returns>
+        [HttpGet]
+        public async Task<IActionResult> GetEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets an event by id.
+        /// </summary>
+        /// <param name="eventId">Event id.</param>
+        /// <returns>Event with all details.</returns>
+        [HttpGet]
+        [Route("/{eventId}")]
+        public async Task<IActionResult> GetEventById(Guid eventId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the number of sold tickets for given event.
+        /// Only available for admins.
+        /// </summary>
+        /// <param name="eventId">Event id.</param>
+        /// <returns>Number of sold tickets for this event.</returns>
+        [HttpGet]
+        [Route("/{eventId}/sales")]
+        public async Task<IActionResult> GetNumberOfSales(Guid eventId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates an event.
+        /// Only available for admins.
+        /// </summary>
+        /// <param name="eventBody">Details of the event to be created.</param>
+        /// <returns>Id of the created event.</returns>
+        [HttpPost]
+        public async Task<IActionResult> CreateEvent([FromBody] Event eventBody)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Only available for customers.
+        /// </summary>
+        /// <param name="eventId">Event id.</param>
+        /// <param name="count">Count of tickets.</param>
+        /// <returns>Ticket id.</returns>
+        [HttpPost]
+        [Route("/{eventId}/purchase")]
+        public async Task<IActionResult> PurchaseTicket(Guid eventId, [FromQuery] int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
