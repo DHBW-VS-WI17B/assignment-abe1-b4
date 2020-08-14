bb9429d1d853ae9fd7f6b97b60e8d933fa67c625
Max Sauer <sauer.max@t-online.de>
2020-08-14 12:30:31 +0200

    Client Main File

--- src/app/TicketStore.Client.App/Program.cs
+++ src/app/TicketStore.Client.App/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using CommandLine;
 
 namespace TicketStore.Client.App
 {
     class Program
     {
+        public class Options
+        {
+            [Option('g', "get", Required = false, HelpText = "Get the specified action from the API.")]
+            public string Get { get; set; }
+
+            [Option('s', "set", Required = false, HelpText = "Set the specified action from the API.")]
+            public string Set { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (o.Get == "events")
+                       {
+
+                           // get from rest api instead of creating the events here
+                           var events = new Dictionary<string, string>()
+                           {
+                               {"event1", "toll" },
+                               {"event2", "auch toll" },
+                           };
+
+                           var jsonString = JsonSerializer.Serialize<Dictionary<string, string>>(events, new JsonSerializerOptions { WriteIndented = true });
+
+                           Console.WriteLine(jsonString);
+
+                       }
+
+                       else if (o.Set == "")
+                       {
+
+                       }
+                   });
         }
+
+       //Method JSON TO CONSOLE
     }
 }
