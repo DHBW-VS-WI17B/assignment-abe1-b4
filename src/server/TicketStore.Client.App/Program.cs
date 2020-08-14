0ee487cc1818f4d22cef1f08b28f637c88bd9adb
skyfrk <github@esslinger.dev>
2020-08-14 11:10:01 +0200

    feat: add client project

---
+++ src/server/TicketStore.Client.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicketStore.Client.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
