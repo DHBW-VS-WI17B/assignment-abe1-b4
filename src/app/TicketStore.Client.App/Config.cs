c9825ebe8f61a130ff50999a377cd7128e5a8214
skyfrk <simon@esslinger.dev>
2020-08-20 15:47:56 +0200

    wip

---
+++ src/app/TicketStore.Client.App/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketStore.Client.App
+{
+    public class Config
+    {
+        public Guid UserId { get; set; }
+
+        public double YearlyBudget { get; set; }
+    }
+}
---
+++ src/app/TicketStore.Client.App/Util/IJsonDataStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketStore.Client.App.Util
+{
+    public interface IJsonDataStore
+    {
+        T Read<T>();
+
+        void Write<T>(T entity);
+    }
+}
