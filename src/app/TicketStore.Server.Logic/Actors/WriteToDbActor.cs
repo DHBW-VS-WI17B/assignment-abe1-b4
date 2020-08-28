92ba1af856cfef4bbf40f7d5aa51ac87b3b8d48c
skyfrk <github@esslinger.dev>
2020-08-29 01:46:29 +0200

    refactor(app): move get remaining budget to extra function

--- src/app/TicketStore.Server.Logic/Actors/WriteToDbActor.cs
+++ src/app/TicketStore.Server.Logic/Actors/WriteToDbActor.cs
@@ -105,6 +105,13 @@ namespace TicketStore.Server.Logic.Actors
                 return;
             }
 
+            if(!_repo.Users.FindByCondition(u => u.Id == msg.UserId).Any())
+            {
+                _logger.Warning("User with id {eventId} does not exist!", msg.UserId);
+                Sender.Tell(new AddTicketsToDbResponse(msg.RequestId, null, $"User with id {msg.UserId} does not exist!"));
+                return;
+            }
+
             if (DateTime.UtcNow > targetEvent.SaleEndDate || DateTime.UtcNow < targetEvent.SaleStartDate)
             {
                 _logger.Warning("There is no active sale for event with id {id}!", msg.EventId);
@@ -113,7 +120,6 @@ namespace TicketStore.Server.Logic.Actors
             }
 
             var soldTickets = _repo.Tickets.FindByCondition(t => t.EventId == msg.EventId).ToImmutableList();
-
             if (soldTickets.Count + msg.TicketCount > targetEvent.MaxTicketCount)
             {
                 _logger.Warning("Can not sell ticket for event with id {id}. Not enough tickets left!", msg.EventId);
@@ -121,17 +127,7 @@ namespace TicketStore.Server.Logic.Actors
                 return;
             }
 
-            double spentOnEvents = 0;
-
-            if (_repo.Tickets.FindByCondition(t => t.UserId == msg.UserId).Any())
-            {
-                var eventIds = _repo.Tickets.FindByCondition(t => t.UserId == msg.UserId).Select(ticket => ticket.EventId).ToList();
-                var events = eventIds.Select(eventId => _repo.Events.FindByCondition(e => e.Id == eventId).FirstOrDefault()).ToList();
-                spentOnEvents = events.Where(e => e.Date.Year == targetEvent.Date.Year).Sum(x => x.PricePerTicket);
-            }
-            var budget = _repo.Users.FindByCondition(u => u.Id == msg.UserId).FirstOrDefault().YearlyBudget;
-            var remainingBudget = budget - spentOnEvents;
-
+            var remainingBudget = GetRemainingBudget(msg.UserId, targetEvent.Date.Year);
             if (msg.TicketCount * targetEvent.PricePerTicket > remainingBudget)
             {
                 _logger.Warning("Can not sell ticket(s) for event with id {id}. Budget is {missingMoney} money units too small.", msg.EventId, (msg.TicketCount * targetEvent.PricePerTicket) - remainingBudget);
@@ -140,7 +136,6 @@ namespace TicketStore.Server.Logic.Actors
             }
 
             var ticketsSoldToUser = soldTickets.Where(t => t.UserId == msg.UserId).ToImmutableList();
-
             if (ticketsSoldToUser.Count + msg.TicketCount > targetEvent.MaxTicketsPerUser)
             {
                 _logger.Warning("Can not sell ticket(s) for event with id {id}. Not enough tickets for user with {id} left!", msg.EventId, msg.UserId);
@@ -166,6 +161,27 @@ namespace TicketStore.Server.Logic.Actors
                 _logger.Error(ex, "Saving tickets failed!");
                 Sender.Tell(new AddTicketsToDbResponse(msg.RequestId, null, "Creating new ticket(s) failed on database level."));
             }
+        }
+
+        /// <summary>
+        /// Gets the remaining budget of a user for a year.
+        /// </summary>
+        /// <param name="userId">User id.</param>
+        /// <param name="year">Year.</param>
+        /// <returns>Remaining budget for given year.</returns>
+        private double GetRemainingBudget(Guid userId, int year)
+        {
+            double spentOnEvents = 0;
+
+            if (_repo.Tickets.FindByCondition(t => t.UserId == userId).Any())
+            {
+                var eventIds = _repo.Tickets.FindByCondition(t => t.UserId == userId).Select(ticket => ticket.EventId).ToList();
+                var events = eventIds.Select(eventId => _repo.Events.FindByCondition(e => e.Id == eventId).FirstOrDefault()).ToList();
+                spentOnEvents = events.Where(e => e.Date.Year == year).Sum(x => x.PricePerTicket);
+            }
+            var budget = _repo.Users.FindByCondition(u => u.Id == userId).FirstOrDefault().YearlyBudget;
+
+            return budget - spentOnEvents;
         }
     }
 }
